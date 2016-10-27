918eb3bcc32320f822d4bc76c84fbd1ec06312fc
Mateus Pinheiro <mateusp@br.ibm.com>
2016-10-27 00:32:25 -0200

    Add some in-file documentation

--- app/assets/scripts/application.js
+++ app/assets/scripts/application.js
@@ -1,2 +1,8 @@
+/**
+ * This is a MANIFEST file. Anything required below will be
+ * bundled into public/application.js
+ *
+ * Require will look into node_modules, and any relative paths.
+ */
 
 require('turbolinks');
