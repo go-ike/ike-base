92c67c819b874484c774ff410febae722f2d3432
Mateus Pinheiro <mateusp@br.ibm.com>
2016-10-28 23:35:36 -0200

    Hone the executables

--- gulpfile.js/tasks/development.js
+++ gulpfile.js/tasks/development.js
@@ -1,11 +1,13 @@
-const gulp       = require('gulp');
-const watch      = require('gulp-watch');
-const batch      = require('gulp-batch');
+const gulp  = require('gulp');
+const watch = require('gulp-watch');
+const batch = require('gulp-batch');
+const chalk = require('chalk');
 
 let developmentTask = function() {
 	watch(['./app/views/**/*.css', './app/assets/styles'], 
 		batch((events, done) => {
 			gulp.start('css', done);
+			console.log(chalk.blue.bold('CSS rebuilt'));
 		})
 	);
 }
