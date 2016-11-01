ef2175a69d8cc58bf6b45ec8ea7a0e56a720ad41
Mateus Pinheiro <mateusp@br.ibm.com>
2016-11-01 14:10:50 -0200

    Add a blank routes file to scaffold the project with

--- config/blank-routes.js
+++ config/blank-routes.js
@@ -6,8 +6,4 @@
 const express = require('express');
 const r       = express.Router();
 
-// Samples
-let SamplesController = new (require('samples.controller'))();
-r.get('/samples', SamplesController.index);
-
 module.exports = r;
